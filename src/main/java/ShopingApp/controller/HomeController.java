aac9bf7064acf524278901c34cd24af33b864ebe
shashipratapsingh <itpro8577@gmail.com>
2025-05-15 17:09:55 +0530

    first commit

---
+++ src/main/java/ShopingApp/controller/HomeController.java
@@ -0,0 +1,20 @@
+package ShopingApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
