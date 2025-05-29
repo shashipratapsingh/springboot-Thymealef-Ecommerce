8d6f4ad9230ad3f2bb6284d2eb7c9850a9e977e7
shashipratapsingh <itpro8577@gmail.com>
2025-05-30 01:12:32 +0530

    admin dashboard

--- src/main/java/ShopingApp/controller/AdminController.java
+++ src/main/java/ShopingApp/controller/AdminController.java
@@ -3,19 +3,24 @@ package ShopingApp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     @GetMapping("/")
     public String index() {
-        return "admin/dashboard"; //return "Admin dashboard route works!"; resolves to src/main/resources/templates/admin/dashboard.html
+        return "admin/dashboard";
     }
 
     @GetMapping("/addProduct")
     public String addProduct() {
-        return "admin/addProduct"; //return "Admin dashboard route works!"; resolves to src/main/resources/templates/admin/dashboard.html
+        return "admin/addProduct";
+    }
+
+    @GetMapping("/category")
+    public String category() {
+        return "admin/category";
     }
 }
+
 
